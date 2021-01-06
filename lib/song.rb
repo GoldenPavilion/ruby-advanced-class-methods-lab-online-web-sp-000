12d320a4215f9a5b83f277f4bc9846ed565f5966
Robert Stine <rcorystine@gmail.com>
2021-01-06 00:16:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,6 +44,7 @@ class Song
   def self.new_from_filename(filename)
     split_filename = filename.chomp(".mp3").split(" - ")
     song = Song.new
-    song.name = split_filename
+    song.name = split_filename[0]
+    song.artist =
   end 
 end
